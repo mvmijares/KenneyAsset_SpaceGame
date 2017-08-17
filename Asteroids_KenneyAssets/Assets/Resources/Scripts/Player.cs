4ef21ba729f7a4337515b0e0837e9a620112ac2a
mvmijares <mvmijares@users.noreply.github.com>
2017-08-17 12:50:36 -0400

    Added Firing

--- Asteroids_KenneyAssets/Assets/Resources/Scripts/Player.cs
+++ Asteroids_KenneyAssets/Assets/Resources/Scripts/Player.cs
@@ -10,33 +10,58 @@ public class Player : MonoBehaviour {
     float vertical;
 
     float speed;
+    Vector2 playerPos;
+
+    public bool debugCondition;
+
+    public GameObject projectile;
+    public Transform bulletSpawnLocation;
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
 
         speed = 10f;
+        debugCondition = false;
     }
     // Use this for initialization
     void Start () {
-	    
+        Physics2D.IgnoreLayerCollision(8, 9); // Change for later
 	}
 	
 	// Update is called once per frame
 	void Update () {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        DebugFunction();
 
-        Debug.Log("Vertical " + vertical + " " + "Horizontal" + horizontal);
-	}
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - new Vector2(transform.position.x, transform.position.y);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, transform.forward);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            FireBullets();  
+        }
+    }
     //Physics
     private void FixedUpdate() {
-        Vector2 playerPos = new Vector2(transform.position.x, transform.position.y);
-
+        playerPos = new Vector2(transform.position.x, transform.position.y);
         rb.MovePosition(playerPos + new Vector2(horizontal, vertical) * Time.deltaTime * speed);
+    }
+    void FireBullets() {
+        Vector3 relativePos = bulletSpawnLocation.position + transform.position; // bulletspawnPosition relative to parent;
+        Instantiate(projectile, relativePos, transform.rotation);
         
     }
-
     //used for follow camera
     private void LateUpdate() {
         
+    }
+
+    void DebugFunction() {
+        if (debugCondition)
+            Debug.Log("Vertical " + vertical + " " + "Horizontal" + horizontal);
+        else {
+            Debug.ClearDeveloperConsole();
+        }
     }
 }
---
+++ Asteroids_KenneyAssets/Assets/Resources/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    Rigidbody2D rb;
+    float speed;
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        speed = 5.0f; // we'll set this up in the player class
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate() {
+        Vector3 force = transform.right * speed;
+        rb.velocity = force; // constant velocity
+    }
+}
